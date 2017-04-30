6f071c1a4b47bfa69a6a03700e914c972ad6e741
undertap <raf@undertap.com>
2017-04-30 03:13:54 +0300

    Add files via upload

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
-const aso = require('aso')('gplay');
+const gplay = require('aso')('gplay');
+const itunes = require('aso')('itunes');
  
 aso.visibility('com.undertap.watchmoviesfree').then(console.log);
